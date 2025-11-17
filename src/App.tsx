b8af188fde8098d6ef983e1cc74295304f5a1c46
Kishan bhati <bhatikaran634@gmail.com>
2025-11-17 17:55:52 +0530

    number solve

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/react">
+      <BrowserRouter >
         <Routes>
           {/* <Route path="/login" element={<Login />} /> */}
 <Route path="/"
